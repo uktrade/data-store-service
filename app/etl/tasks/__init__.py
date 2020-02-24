0147cd1321de0c29c507ad1fb926bfe592bcc559
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-02-24 10:53:59 +0000

    Moved into tasks dir and refactored

---
+++ app/etl/tasks/__init__.py
@@ -0,0 +1,32 @@
+import sqlalchemy.exc
+from flask import current_app as flask_app
+
+
+class ETLTask:
+    name = ''
+
+    def __init__(self, sql, model, drop_table=False):
+        self.drop_table = drop_table
+        self.sql = sql
+        self.model = model
+
+    def __call__(self, *args, **kwargs):
+        connection = flask_app.db.engine.connect()
+        transaction = connection.begin()
+        try:
+            if self.drop_table is True:
+                self.model.drop_table()
+            self.model.create_table()
+            result = connection.execute(self.sql)
+            transaction.commit()
+            return {
+                'status': 200,
+                'rows': result.rowcount,
+                'task': self.name,
+            }
+        except sqlalchemy.exc.ProgrammingError as err:
+            transaction.rollback()
+            flask_app.logger.error(f'Error running task, "{self.name}". Error: {err}')
+            raise err
+        finally:
+            connection.close()
--- app/etl/tasks/etl_wits_clean.py
+++ app/etl/tasks/etl_wits_clean.py
@@ -1,3 +1,5 @@
+from app.etl.tasks import ETLTask
+
 from app.db.models.external import (
     Product0201,
     Product040110,
@@ -8,40 +10,6 @@ HS_MODEL_MAPPING = {
     '0201': Product0201,
     '040110': Product040110
 }
-
-
-import sqlalchemy.exc
-from flask import current_app as flask_app
-
-
-class ETLTask:
-    name = ''
-
-    def __init__(self, sql, model, drop_table=False):
-        self.drop_table = drop_table
-        self.sql = sql
-        self.model = model
-
-    def __call__(self, *args, **kwargs):
-        connection = flask_app.db.engine.connect()
-        transaction = connection.begin()
-        try:
-            if self.drop_table is True:
-                self.model.drop_table()
-            self.model.create_table()
-            result = connection.execute(self.sql)
-            transaction.commit()
-            return {
-                'status': 200,
-                'rows': result.rowcount,
-                'task': self.name,
-            }
-        except sqlalchemy.exc.ProgrammingError as err:
-            transaction.rollback()
-            flask_app.logger.error(f'Error running task, "{self.name}". Error: {err}')
-            raise err
-        finally:
-            connection.close()
 
 
 class CleanWITSProduct:
@@ -65,14 +33,16 @@ class CleanWITSProduct:
             """)
         return sql
 
-    def insert_rows_with_empty_rates(self):
-        sql = f"""
-
-        with product_combos_1 as (
+    def get_partner_reporter_combinations_for_each_year(self, query_name):
+        return f"""
+        {query_name} as (
              {'UNION ALL'.join(self.year_fill())}
-        ),
-         
-        product_combos_2 as (
+        )
+        """
+
+    def get_rate_averages(self, query_name, query_from):
+        return f"""
+        {query_name} as (
             select distinct
                p.*,
                case 
@@ -87,7 +57,7 @@ class CleanWITSProduct:
                case 
                  when mfn.simple_average is not null then mfn.simple_average::text else 'NA'::text
                end as mfn_rate
-              from product_combos_1 p 
+              from {query_from} p 
             left join {self.db_model.get_fq_table_name()} bnd on p.partner = bnd.partner and 
                  p.reporter = bnd.reporter and bnd.duty_type = 'BND' and p.year::text = bnd.tariff_year::text
             left join {self.db_model.get_fq_table_name()} prf on p.partner = prf.partner and 
@@ -96,26 +66,40 @@ class CleanWITSProduct:
                 p.reporter = mfn.reporter and mfn.duty_type = 'MFN' and p.year::text = mfn.tariff_year::text
             left join {self.db_model.get_fq_table_name()} ash on p.partner = ash.partner and 
                 p.reporter = ash.reporter and ash.duty_type = 'AHS' and p.year::text = ash.tariff_year::text
-        ),
-        
-        product_combos_3 as (
-           select 
-           p.reporter as reporter,
-           AVG (p.app_rate::float) as country_average
-           from product_combos_2 p
-           where p.app_rate != 'NA'
-           group by p.reporter
-        ),
-        
-        product_combos as (
-            select 
+        )
+        """
+
+    def get_country_average(self, query_name, query_from):
+        return f"""
+        {query_name} as (
+            select
+            p.reporter,
+            AVG (p.app_rate::float) as country_average
+            from {query_from} p
+            where
+            p.app_rate != 'NA'
+            group
+            by
+            p.reporter
+        )
+        """
+
+    def get_world_average(self, query_name, query_from):
+        return f"""
+        {query_name} as (
+            select
             p.*,
             s.country_average as country_average,
-            (select AVG(app_rate::float) from product_combos_2 where app_rate != 'NA') as world_average
-            from product_combos_2 p
-            left join product_combos_3 s on s.reporter = p.reporter
+            (select AVG(app_rate::float) from {query_from} where app_rate != 'NA') 
+            as world_average
+            from {query_from} p
+            left join country_average s
+            on s.reporter = p.reporter
         )
-  
+        """
+
+    def insert_data_to_L1(self, query_from):
+        return f"""
         insert into {WITSCleanedData.get_fq_table_name()} (
             product,
             reporter,
@@ -139,13 +123,27 @@ class CleanWITSProduct:
             bnd_rate,
             country_average,
             world_average 
-        from product_combos
-    """
+        from {query_from}
+        """
+
+    def transform_data(self):
+        initial_query_name = 'base_product'
+        country_average_table_name = 'country_average'
+        intermediate_table_name = 'products_with_rate_averages'
+        final_table = 'populated_products'
+
+        sql = f"""
+        with {self.get_partner_reporter_combinations_for_each_year(initial_query_name)},
+        {self.get_rate_averages(intermediate_table_name, initial_query_name)},
+        {self.get_country_average(country_average_table_name, intermediate_table_name)},
+        {self.get_world_average(final_table, intermediate_table_name)}
+        {self.insert_data_to_L1(final_table)}
+
+        """
         obj = ETLTask(
             sql, WITSCleanedData, drop_table=True
         )
         obj()
 
     def main(self):
-        self.insert_rows_with_empty_rates()
-
+        self.transform_data()
