8009e47b034ee36d282d66015cfe934bd46e89b0
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2020-02-28 15:43:38 +0000

    Fix flake8 errors

--- app/api/views/world_bank_tariffs.py
+++ app/api/views/world_bank_tariffs.py
@@ -1,12 +1,9 @@
-from flask import current_app as flask_app
-from flask import request
-from flask.blueprints import Blueprint
 import pandas as pd
+from flask import current_app as flask_app
+from flask.blueprints import Blueprint
 
-from app.api.utils import response_orientation_decorator, to_web_dict
+from app.api.utils import to_web_dict
 from app.api.views import ac, json_error
-from app.db.models.external import ONSPostcodeDirectoryL1
-from app.etl.etl_ons_postcode_directory import ONSPostcodeDirectoryPipeline
 
 api = Blueprint(name="world_bank_tariffs_api", import_name=__name__)
 
@@ -16,16 +13,6 @@ api = Blueprint(name="world_bank_tariffs_api", import_name=__name__)
 @ac.authentication_required
 @ac.authorization_required
 def get_world_bank_tariffs():
-    pagination_size = flask_app.config['app']['pagination_size']
-    next_id = request.args.get('next-id')
-
-    where = ''
-    values = []
-
-    if next_id is not None:
-        where = 'where id >= %s'
-        values = [next_id]
-
     df = pd.DataFrame()
     next_ = None
     web_dict = to_web_dict(df.iloc[:, 1:])
