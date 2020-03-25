fb546d3447f2797512ba97755dc65aba6daf5e33
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2020-03-25 14:25:00 +0000

    PipelinePaginatedListView added

--- app/api/views/world_bank_bound_rates.py
+++ app/api/views/world_bank_bound_rates.py
@@ -1,12 +1,8 @@
-from data_engineering.common.views import ac, base, json_error
-
+from app.api.views.base import PipelinePaginatedListView
 from app.db.models.external import WorldBankBoundRateL1
 from app.etl.etl_world_bank_tariff import WorldBankBoundRatesPipeline
 
 
-class WorldBankBoundRatesListView(base.PaginatedListView):
-
-    decorators = [json_error, ac.authentication_required, ac.authorization_required]
-
-    pipeline = WorldBankBoundRatesPipeline
+class WorldBankBoundRatesListView(PipelinePaginatedListView):
+    pipeline_column_types = WorldBankBoundRatesPipeline._l1_data_column_types
     model = WorldBankBoundRateL1
--- app/api/views/world_bank_tariffs.py
+++ app/api/views/world_bank_tariffs.py
@@ -1,23 +1,13 @@
-from data_engineering.common.views import ac, base, json_error
-
+from app.api.views.base import PipelinePaginatedListView
 from app.db.models.external import WorldBankTariffL0, WorldBankTariffTransformL1
 from app.etl.etl_world_bank_tariff import WorldBankTariffPipeline, WorldBankTariffTransformPipeline
 
 
-class WorldBankTariffTransformListView(base.PaginatedListView):
-
-    decorators = [json_error, ac.authentication_required, ac.authorization_required]
-
-    pipeline = WorldBankTariffTransformPipeline
+class WorldBankTariffTransformListView(PipelinePaginatedListView):
+    pipeline_column_types = WorldBankTariffTransformPipeline._l1_data_column_types
     model = WorldBankTariffTransformL1
 
 
-class WorldBankTariffListView(base.PaginatedListView):
-
-    decorators = [json_error, ac.authentication_required, ac.authorization_required]
-
-    pipeline = WorldBankTariffPipeline
+class WorldBankTariffListView(PipelinePaginatedListView):
+    pipeline_column_types = WorldBankTariffPipeline._l0_data_column_types
     model = WorldBankTariffL0
-
-    def get_fields(self):
-        return self.get_field_types_from_column_types(self.pipeline._l0_data_column_types)
