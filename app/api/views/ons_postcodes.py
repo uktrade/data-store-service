b28c3d4a2636380e88fa8173c90b4209ced9db0a
David Voong <voong.david@gmail.com>
2020-04-27 09:43:47 +0100

    remove print

--- app/api/views/ons_postcodes.py
+++ app/api/views/ons_postcodes.py
@@ -2,8 +2,6 @@ from app.api.views.base import PipelinePaginatedListView
 from app.db.models.external import ONSPostcodeDirectoryL1
 from app.etl.organisation.ons import ONSPostcodeDirectoryPipeline
 
-print('test')
-
 
 class OnsPostcodeListView(PipelinePaginatedListView):
     pipeline_column_types = ONSPostcodeDirectoryPipeline._l1_data_column_types
