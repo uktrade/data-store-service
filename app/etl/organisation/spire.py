ddd6d3b47bb91017f209ec5cca9bf62deaa50786
David Voong <voong.david@gmail.com>
2020-04-27 11:26:45 +0100

    formatting

--- app/etl/organisation/spire.py
+++ app/etl/organisation/spire.py
@@ -1,12 +1,12 @@
 import app.db.models.external as models
-from app.etl.rebuild_schema import RebuildSchemaPipeline
+from app.etl.pipeline_type.rebuild_schema import RebuildSchemaPipeline
 
 
 class SPIREPipeline(RebuildSchemaPipeline):
     organisation = 'spire'
     schema = models.SPIRE_SCHEMA_NAME
 
-    
+
 class SPIRECountryGroupPipeline(SPIREPipeline):
 
     dataset = models.SPIRECountryGroup.__tablename__
@@ -17,3 +17,9 @@ class SPIRECountryGroupEntryPipeline(SPIREPipeline):
 
     dataset = models.SPIRECountryGroupEntry.__tablename__
     sql_alchemy_model = models.SPIRECountryGroupEntry
+
+
+class SPIRERefCountryMappingPipeline(SPIREPipeline):
+
+    dataset = models.SPIRERefCountryMapping.__tablename__
+    sql_alchemy_model = models.SPIRERefCountryMapping
