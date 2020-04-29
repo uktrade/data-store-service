64c3eb84c9825c1efe2385b98c9116f0b1d8272b
David Voong <voong.david@gmail.com>
2020-04-29 11:50:39 +0100

    add handling of null values

--- tests/etl/rebuild_schema/test_RebuildSchemaPipeline.py
+++ tests/etl/rebuild_schema/test_RebuildSchemaPipeline.py
@@ -61,3 +61,26 @@ class TestProcess:
         expected = [mock.call(c='0', d='1'), mock.call(c='2', d='3')]
 
         assert calls == expected
+
+    def test_null_values_converted_to_none(self, app_with_db, mock_sql_alchemy_model):
+
+        dbi = mock.Mock()
+        fieldnames = ['a', 'b']
+        rows = [[0, 'null'], [1, 'apple'], [2, 'pineapple'], ]
+        data = convert_to_csv_bytes(fieldnames, rows)
+        file_info = mock.Mock()
+        file_info.data.read.return_value = data
+
+        pipeline = RebuildSchemaPipeline(dbi)
+        pipeline.null_values = ['null', 'pineapple']
+        pipeline.process(file_info)
+
+        calls = mock_sql_alchemy_model.call_args_list
+        expected = [
+            mock.call(a='0', b=None),
+            mock.call(a='1', b='apple'),
+            mock.call(a='2', b=None)
+        ]
+
+        assert calls == expected
+        
